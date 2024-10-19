f06de269abed332ad5b73f513c0d2115ec5514d9
Epa26 <55296078+Epa26@users.noreply.github.com>
2024-10-19 15:05:00 -0300

    implementacion p3 - Listar calificaciones curso y estudiante

---
+++ data.py
@@ -0,0 +1,11 @@
+# Datos simulados
+
+calificaciones_simuladas = [
+    {"id_estudiante": 1, "id_curso": "MAT-021", "calificacion": 95},
+    {"id_estudiante": 1, "id_curso": "FIS-100", "calificacion": 89},
+    {"id_estudiante": 2, "id_curso": "MAT-021", "calificacion": 72},
+    {"id_estudiante": 2, "id_curso": "IWI-231", "calificacion": 88},
+    {"id_estudiante": 3, "id_curso": "MAT-021", "calificacion": 65},
+    {"id_estudiante": 3, "id_curso": "FIS-100", "calificacion": 78},
+    {"id_estudiante": 3, "id_curso": "IWI-231", "calificacion": 92},
+]
