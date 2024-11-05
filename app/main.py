52719572a382604715f0f6333db29d28c7d79c18
Epa26 <55296078+Epa26@users.noreply.github.com>
2024-11-05 17:14:47 -0300

    interface v1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routes import grades
 import logging
+from reactpy import html, component
+from reactpy.backend.fastapi import configure
 
 app = FastAPI()
 
@@ -9,7 +11,64 @@ app.include_router(grades.router, prefix="/api/v1")
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
-@app.get("/")
-async def root():
-    logging.info("👋 Hello world (end-point)!")
-    return {"message": "Grade Management API is running!"}
+def Module(props):
+    return html.li(
+        {"key": props['id'], "style": modules_style},
+        f"{props['text']} - {props['color']}",
+        html.button({"style": button_style, "onclick": lambda: handle_click(props['id'])}, f"{props['action']}")
+    )
+
+
+modules_style = {
+    "background": "skyblue",
+    "padding": "1rem",
+    "border": "1px solid black",
+    "margin": "2rem",
+}
+button_style = {
+    "margin-left": "1rem",
+    "padding": "0.5rem",
+    "background": "yellow",
+    "border": "none",
+    "cursor": "pointer",
+}
+
+
+def handle_click(module_id):
+    print(f"Modulo con id {module_id} fue seleccionado.")
+
+@component
+def ModulesList():
+    modules = [
+        {"id": 0, "text": "Registrar calificación", "color": "yellow", "action": "Registar"},
+        {"id": 1, "text": "Ver calificaciones por parallel_id", "color": 0, "action": "Visualizar"},
+        {"id": 2, "text": "Ver calificaciones por course_id", "color": 0, "action": "Visualizar"},
+        {"id": 3, "text": "Consultar calificación por grade_id", "color": 0, "action": "Consultar"},
+        {"id": 4, "text": "Eliminar calificación por grade_id", "color": 2, "action": "Eliminar"},
+        {"id": 5, "text": "Ver calificaciones de un estudiante por student_id", "color": 2, "action": "Visualizar"},
+        {"id": 6, "text": "Ver calificaciones por course_id", "color": 1, "action": "Visualizar"},
+        {"id": 7, "text": "Actualizar calificación por course_id", "color": 1, "action": "Actualizar"},
+    ]
+    return html.ul([Module(module) for module in modules])
+
+
+
+
+@component
+def App():
+    return html.div(
+        {"style": {
+            "display": "flex",
+            "flex-direction": "column",
+            "align-items": "center",  # Centra el contenido horizontalmente
+            "justify-content": "center",  # Centra el contenido verticalmente
+            "min-height": "100vh",  #  Altura de la ventana
+            "text-align": "center",
+            "font-family": "Arial, sans-serif"
+        }},
+        html.h1("Módulo de Calificaciones"),
+        ModulesList()
+    )
+configure(app, App)
+
+
